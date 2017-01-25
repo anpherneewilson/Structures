cefdcf322da9d2e6dbb4cbd0479fd3bce191d130
Anphernee <anpherneew@gmail.com>
2017-01-24 23:25:12 -0500

    problem 2 corrections

--- Problem2.rkt
+++ Problem2.rkt
@@ -1,7 +1,7 @@
 (define (sum-up-numbers-simple L)
   (cond
     ((null? L) 0)
-    ((integer? (car L)) (+ (car L) (sum-up-numbers-simple (cdr L))))
+    ((integer? (car L)) (+ (sum-up-numbers-simple (cdr L)) (car L)))
     (else (sum-up-numbers-simple (cdr L)))
     )
   )
