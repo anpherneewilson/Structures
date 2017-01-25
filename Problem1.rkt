16d56e3b920c80f9c38ff4c300ec3c4d53b3f3f2
Anphernee <anpherneew@gmail.com>
2017-01-24 23:01:04 -0500

    problem1 corrected logic

--- Problem1.rkt
+++ Problem1.rkt
@@ -1,7 +1,7 @@
 (define (reverse-general L)
-  (if
-    (null? L)
-    '()
-    (append (reverse-general (cdr L)) (list (car L)))
+  (cond
+    ((null? L) '())
+    ((list? (car L)) (append (reverse-general (cdr L)) (list (reverse-general (car L)))))
+    (else (append (reverse-general (cdr L)) (list (car L))))
     )
   )
